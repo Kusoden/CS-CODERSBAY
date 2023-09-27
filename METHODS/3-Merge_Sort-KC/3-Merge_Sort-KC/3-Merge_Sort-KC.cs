000c6cd88101139c96cbf762fda10205f1aa893a
Farzān <Ferzanden@gmail.com>
2023-09-27 11:56:52 +0200

    Write method
    Write method should be triggered at end to Write the new version.
    2 methods Needed for sorting

--- METHODS/3-Merge_Sort-KC/3-Merge_Sort-KC/3-Merge_Sort-KC.cs
+++ METHODS/3-Merge_Sort-KC/3-Merge_Sort-KC/3-Merge_Sort-KC.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] list = { 300, 11, 53, 144, 74, 1, 27, 9, 220, 86, 13, 75, 23, 73, 84, 16, 36, 86, 82, 400, 73 };
+Console.WriteLine("Original List: ");
+Write(list);
+
+static void Write(int[] list)
+{
+    foreach (int element in list) Console.Write(element.ToString() + " ");
+}
+
+
