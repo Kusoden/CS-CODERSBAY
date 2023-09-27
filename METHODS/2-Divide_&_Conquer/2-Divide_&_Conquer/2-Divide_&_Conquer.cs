7485e745ef7bb91b325b33a834dd8d6a54973af7
Farzān <Ferzanden@gmail.com>
2023-09-27 11:02:14 +0200

    Finished Divide and Conquer with Recursion
    finding the max value inside an Array using Divide and Conquer by recursion

---
+++ METHODS/2-Divide_&_Conquer/2-Divide_&_Conquer/2-Divide_&_Conquer.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+int[] list = { 300, 11, 53, 144, 74, 1, 27, 9, 220, 86, 13, 75, 23, 73, 84, 16, 36, 86, 82, 400, 73 };
+Console.WriteLine("Max Value in the pocket: " + Findmax(list, 0, list.Length - 1));
+
+
+static int Findmax(int[] list, int BeginningOfList, int endOfList)
+{
+    if (BeginningOfList == endOfList) /* if there is only one element in List*/
+        return list[BeginningOfList];
+    int middleOfList = (BeginningOfList + endOfList) / 2;
+
+    return Math.Max(Findmax(list, BeginningOfList, middleOfList), Findmax(list, middleOfList + 1, endOfList));
+}
