9c7079f2c7c9c7ee5c16d0d13550eb28d788dc95
Farzān <Ferzanden@gmail.com>
2023-10-05 15:12:06 +0200

    Stack using singly linked list

---
+++ 6-OOP_2/StackwithLinkedList/StackwithLinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace StackwithLinkedList;
+
+class Program
+{
+
+    static void Main()
+    {
+        Stack obj = new();
+
+        obj.Push("Destiny");
+        obj.Push("Warcraft 3");
+        obj.Push("League of Legends");
+        obj.Push("Forza Horizon");
+        obj.Push("Final Fantasy XV");
+        obj.Push("Assassin's Creed");
+        obj.Push("Baldur's Gate");
+        obj.Push("Victoria 3");
+
+        Console.WriteLine("size of the Library");
+
+        Console.WriteLine(obj.Size()+"\n");
+        
+
+        obj.Display();
+
+        Console.WriteLine("\nGame at the top: {0}\n", obj.Peek());
+
+        obj.Pop();
+        obj.Pop();
+
+        Console.WriteLine("\nafter 2 popings Library:");
+        obj.Display();
+
+        Console.WriteLine("\nGame at the top: {0}\n", obj.Peek());
+
+        Console.WriteLine("size of the Library");
+
+        Console.WriteLine(obj.Size() + "\n");
+
+        string[] poppedGames = obj.Pop(2);
+        Console.WriteLine("PoppedGames: ");
+
+        foreach(var game in poppedGames) Console.WriteLine(game);
+    }
+}
