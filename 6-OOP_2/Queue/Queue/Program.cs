673d09a599d9bb6944a82a9dc7449ee4351e916c
Farzān <Ferzanden@gmail.com>
2023-10-06 12:13:12 +0200

    Queue using LinkedLIst

--- 6-OOP_2/Queue/Queue/Program.cs
+++ 6-OOP_2/Queue/Queue/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace QueueDoubleLinkedList;
+class Program
+{
+    static void Main()
+    {
+        QueueDoubleLinkedList theQueue = new();
+
+        theQueue.Enqueue(1);
+        theQueue.Enqueue(2);
+        theQueue.Enqueue(3);
+        theQueue.Enqueue(4);
+        theQueue.Enqueue(5);
+        theQueue.Enqueue(6);
+        theQueue.Enqueue(7);
+        theQueue.Enqueue(8);
+        theQueue.Enqueue(9);
+        theQueue.Enqueue(10);
+
+        Console.WriteLine(theQueue.ToString()+ "\n\n");
+        Console.WriteLine("DEQUEUED NUMBER: " + theQueue.Dequeue());
+
+
+        Console.WriteLine("\n\nAfter DEQUEUING:"+ theQueue.ToString());
+        int[] Dequeue = theQueue.Dequeue(2);
+
+
+        Console.WriteLine("Removed and stored numbers: ");
+        foreach ( int numb in Dequeue ) Console.Write("|"+numb);
+
+        Console.WriteLine("\n\nAfter DEQUEUING and STORING:" + theQueue.ToString());
+    }
+}
