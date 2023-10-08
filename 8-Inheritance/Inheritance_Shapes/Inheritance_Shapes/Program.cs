0838fb716fc58a2ac9d76cec3bbc75bb88410aa6
Farzān <Ferzanden@gmail.com>
2023-10-08 20:28:34 +0200

    Inheritance with Shapes
    only its TEST is left

---
+++ 8-Inheritance/Inheritance_Shapes/Inheritance_Shapes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Inheritance_Shapes;
+
+class Program
+{
+    static void Main()
+    {
+        Shape circle = new Circle("Green", true, 5.0);
+        Shape rectangle = new Rectangle("White", false, 0, 0);
+        Shape square = new Square("Red", true, 11.0);
+
+        Console.WriteLine($"Circle Area: {circle.GetArea()}, Perimeter: {circle.GetPerimeter()}\nit's {circle.Color} colored\nis it filled? Answer: {circle.IsFilled}\n");
+        Console.WriteLine($"\nRectangle Area: {rectangle.GetArea()}, Perimeter: {rectangle.GetPerimeter()}\nit's {rectangle.Color} colored\nis it filled? Answer: {rectangle.IsFilled}\n");
+        Console.WriteLine($"Square Area: {square.GetArea()}, Perimeter: {square.GetPerimeter()}\nit's {square.Color} colored\nis it filled? Answer: {square.IsFilled}");
+    }
+}
---
+++ 8-Inheritance/Inheritance_Shapes/Inheritance_Shapes/Shapes.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Shapes;
+
+public abstract class Shape
+{
+    public string Color { get; set; }
+    public bool IsFilled { get; set; }
+
+    public Shape(string color, bool isFilled)
+    {
+        Color = color;
+        IsFilled = isFilled;
+    }
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+}
+
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(string color, bool isFilled, double radius) : base(color, isFilled)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("\nERROR: Radius must be higher than 0.\n");
+        else 
+            Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(string color, bool isFilled, double width, double height) : base(color, isFilled)
+    {
+        if (width <= 0)
+            throw new ArgumentException("\nERROR: Width must be higher than 0.\n");
+        else if (height <= 0)
+            throw new ArgumentException("\nHeight must be higher than 0.\n");
+        else
+        {
+            Width = width;
+            Height = height;
+        }
+        
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (Width + Height);
+    }
+}
+
+public class Square : Rectangle
+{
+    public double Side { get; set; }
+
+    public Square(string color, bool isFilled, double side) : base(color, isFilled, side, side)
+    {
+        if (side <= 0)
+            throw new ArgumentException("Side length must be higher than 0.");
+        else 
+            Side = side;
+    }
+
+    public override double GetArea()
+    {
+        return Side * Side;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 4 * Side;
+    }
+}
